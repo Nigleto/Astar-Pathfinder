0c009f5557efc8648338cac73afdd4e374b26153
Nigel P <58761175+Nigleto@users.noreply.github.com>
2021-03-18 21:39:21 -0700

    banana

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ class Spot:
         return self.color == ORANGE
 
     def is_end(self):
-        return self.color == PURPLE
+        return self.color == TURQUOISE
 
     def reset(self):
         self.color = WHITE
@@ -73,7 +73,15 @@ class Spot:
         pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
     def update_neighbors(self, grid):
-        pass
+        self.neighbors = []
+        if self.row < self.total_rows - 1 and not grid[self.row - 1][self.col].is_barrier(): # DOWN
+            self.neighbors.append(grid[self.row + 1][self.col])
+        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier(): # UP
+            self.neighbors.append(grid[self.row - 1][self.col])
+        if self.col < self.total_rows - 1 and not grid[self.row - 1][self.col + 1].is_barrier(): # LEFT
+            self.neighbors.append(grid[self.row][self.col + 1])
+        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier(): # RIGHT
+            self.neighbors.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
         return False
@@ -166,6 +174,10 @@ def main(win, width):
                     start == None
                 elif spot == end:
                     end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not started:
+                    pass
 
     pygame.quit()
 
